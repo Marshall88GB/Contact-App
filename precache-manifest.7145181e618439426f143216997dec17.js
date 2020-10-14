ccc55d472ac758df80d0e890d9a8f43245c0d015
Marshall88GB <gasic.boban@gmail.com>
2020-10-14 23:06:11 +0200

    Updates

---
+++ precache-manifest.7145181e618439426f143216997dec17.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bdb3dc1c4c6274b50be07c1ba6c691fd",
+    "url": "/Contact-App/index.html"
+  },
+  {
+    "revision": "56111265856bce1f50cd",
+    "url": "/Contact-App/static/css/main.17e0d4dd.chunk.css"
+  },
+  {
+    "revision": "e0aab831460728306cfe",
+    "url": "/Contact-App/static/js/2.11a0c184.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/Contact-App/static/js/2.11a0c184.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "56111265856bce1f50cd",
+    "url": "/Contact-App/static/js/main.389b34c3.chunk.js"
+  },
+  {
+    "revision": "9a7dcbcc252a5a2876ab",
+    "url": "/Contact-App/static/js/runtime-main.f027fc39.js"
+  }
+]);
